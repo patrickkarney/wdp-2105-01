247afdc89d5c1320f3745fd3b1bfd32e2b1724f2
Sebastian Sikora <ssikora80@gmail.com>
2021-05-13 21:00:52 +0200

    Add Search scroll menu

--- src/components/features/ProductSearch/ProductSearch.js
+++ src/components/features/ProductSearch/ProductSearch.js
@@ -10,9 +10,20 @@ const ProductSearch = () => (
   <form action='' className={styles.root}>
     <div className={styles.category}>
       <FontAwesomeIcon className={styles.icon} icon={faListUl} />
-      <select name='' id=''>
-        <option value=''>Select a category</option>
-      </select>
+      <div className={styles.selectField}>
+        <ul>
+          <li>
+            Select a category
+            <ul>
+              <li>Bed</li>
+              <li>Chair</li>
+              <li>Sofa</li>
+              <li>Table</li>
+              <li>Dining</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
       <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
     </div>
     <div className={styles.searchField}>
